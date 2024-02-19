b57549bd41d05e45a6496705b4fa9818e33beb91
daoverton <andrewoverton123@gmail.com>
2024-02-18 22:20:26 -0600

    Limited hacknet node purchases

--- src/hacknet.ts
+++ src/hacknet.ts
@@ -1,29 +1,38 @@
 /** @param {NS} ns */
-export async function main(ns: NS) {
+export async function main(ns: NS): Promise<void> {
   while (true) {
     await ns.sleep(100);
     const hackNet = ns.hacknet
     const numHacknetNodes = hackNet.numNodes()
 
+    const hacknetProduction = [...Array(numHacknetNodes).keys()]
+    .map((nodeNumber: number) => {
+      const stats = hackNet.getNodeStats(nodeNumber);
+
+      return stats.production;
+    })
+    .reduce(
+      (accumulator: number, currentProduction: number) => accumulator + currentProduction, 0
+    )
+
     const nodePurchaseCost = hackNet.getPurchaseNodeCost()
 
-    if (hackNet.maxNumNodes() >= numHacknetNodes && nodePurchaseCost < ns.getServerMoneyAvailable('home')){
+    if (hackNet.maxNumNodes() >= numHacknetNodes && nodePurchaseCost < ns.getServerMoneyAvailable('home') && nodePurchaseCost < hacknetProduction / 3600){
       hackNet.purchaseNode()
     }
 
     for (let nodeNumber = 0; nodeNumber < numHacknetNodes; nodeNumber++) {
-      const stats = hackNet.getNodeStats(nodeNumber)
       const coreCost = hackNet.getCoreUpgradeCost(nodeNumber, 1)
       const ramCost = hackNet.getRamUpgradeCost(nodeNumber, 1)
       const levelCost = hackNet.getLevelUpgradeCost(nodeNumber, 1)
 
-      if (coreCost < ns.getServerMoneyAvailable('home') && coreCost < nodePurchaseCost) {
+      if (coreCost < ns.getServerMoneyAvailable('home')) {
         hackNet.upgradeCore(nodeNumber, 1)
       }
-      if (ramCost < ns.getServerMoneyAvailable('home') && ramCost < nodePurchaseCost) {
+      if (ramCost < ns.getServerMoneyAvailable('home')) {
         hackNet.upgradeRam(nodeNumber, 1)
       }
-      if (levelCost < ns.getServerMoneyAvailable('home') && levelCost < nodePurchaseCost) {
+      if (levelCost < ns.getServerMoneyAvailable('home')) {
         hackNet.upgradeLevel(nodeNumber, 1)
       }
     }
